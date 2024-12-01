1b1bd1db4a0aa8e9d054c9ad27da6f76f7fb2e12
Najat zaidane <najatzaidane2002@outlook.com>
2024-12-01 15:55:22 +0100

    BLOCK DTOs

--- class-service/src/main/java/com/classeye/classservice/dto/BlockDTO.java
+++ class-service/src/main/java/com/classeye/classservice/dto/BlockDTO.java
@@ -1,10 +1,20 @@
 package com.classeye.classservice.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 /**
  * @author Najat
  */
-public record BlockDTO( Long id, String name, String description, int floor, int roomCount, List<SalleDTO> salles) {
+public record BlockDTO( Long id,
+                        String name,
+                        String description,
+                        int roomCount,
+                        List<SalleDTO> salles
+                        )
+{
 
 }
