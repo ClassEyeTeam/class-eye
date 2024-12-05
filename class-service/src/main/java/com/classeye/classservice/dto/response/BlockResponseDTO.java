685e7d7475c1d320529cf8c5d3229c4663aaad51
Najat zaidane <najatzaidane2002@outlook.com>
2024-12-05 19:59:59 +0100

    Mise à jour du DTO BlockResponse

--- class-service/src/main/java/com/classeye/classservice/dto/response/BlockResponseDTO.java
+++ class-service/src/main/java/com/classeye/classservice/dto/response/BlockResponseDTO.java
@@ -12,6 +12,5 @@ public record BlockResponseDTO(Long id,
                                 String description,
                                 int roomCount,
                                List<SalleDTO> salles
-
 ) {
 }
