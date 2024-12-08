7d03a82eda6c381dc53f186ea222a42537003bd9
Najat zaidane <najatzaidane2002@outlook.com>
2024-12-08 12:08:38 +0100

    BlockMapper  update 2  !

--- class-service/src/main/java/com/classeye/classservice/mapper/BlockMapper.java
+++ class-service/src/main/java/com/classeye/classservice/mapper/BlockMapper.java
@@ -5,12 +5,24 @@ import com.classeye.classservice.dto.request.BlockCreateDTO;
 import com.classeye.classservice.dto.response.BlockResponseDTO;
 import com.classeye.classservice.entity.Block;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 /**
  * @author Najat
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {SalleMapper.class})
 public interface BlockMapper {
+    // this is a way to create a mapper instance without using the new keyword and it is thread-safe
+    BlockMapper INSTANCE = Mappers.getMapper(BlockMapper.class);
+
+    @Mapping(target = "salles", source = "salles")
     BlockResponseDTO toBlockDTO(Block block);
+
+    @Mapping(target = "id", ignore = true)
     Block toBlock(BlockCreateDTO blockCreateDTO);
+
+//    @Mapping(target = "salles", source = "salles")
+//    BlockSallesDTO toSallesDTO(Block block);
+
 }
