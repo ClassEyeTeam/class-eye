8f314e168bb7d9a9a4c79bf018a3cfa3ff3a7e01
MohammedSadok <mohammed.sadok.2003@gmail.com>
2024-12-11 21:34:02 +0100

    fix cors

--- university-service/src/main/java/com/classeye/universityservice/config/Config.java
+++ university-service/src/main/java/com/classeye/universityservice/config/Config.java
@@ -1,10 +1,7 @@
 package com.classeye.universityservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author moham
@@ -13,17 +10,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableJpaAuditing
 class Config {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","http://localhost:8088")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
 }
---
+++ university-service/src/main/java/com/classeye/universityservice/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.classeye.universityservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author moham
+ **/
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
